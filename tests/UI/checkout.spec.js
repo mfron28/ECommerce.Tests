18b769cdac41c00a48ea49cf50432ff762d0f839
Madalina Fron <madalinafron@Madalinas-MacBook-Air.local>
2026-07-20 16:14:51 +0300

    Add checkout test for incomplete shipping address.

--- tests/UI/checkout.spec.js
+++ tests/UI/checkout.spec.js
@@ -30,3 +30,34 @@ test('User go to checkout page', async ({ loggedIn, page }) => {
 
   await expect(page).toHaveURL(/\/orders\//);
 });
+
+test('Place order with incomplete address',async({loggedIn,page})=>{
+    const productsPage=loggedIn.getProductsPage();
+    const checkoutPage=loggedIn.getCheckoutPage();
+    const cartPage=loggedIn.getCartPage();
+
+    await productsPage.gotoProductsListing();
+    await productsPage.searchProduct('Flip Flops');
+    await productsPage.selectProduct('Flip Flops');
+    await  productsPage.addProductToCart();
+    await  productsPage.goToCartPage();
+
+    await cartPage.goToCheckoutFromCartPage();
+    await expect(page.getByRole('heading', { name: 'Checkout', level: 1 })).toBeVisible();
+  
+    await checkoutPage.fillCheckoutPage({
+        fullName: ' ',
+        address1: '',
+        cityPlace: '',
+        region: 'Iasi',
+        code: '02101',
+        country: 'Romania',
+        shipping: 'EU',
+        coupon: '',
+      });
+
+    await expect(page).toHaveURL(/\/checkout/);
+    await expect(page.locator('.error-banner')).toBeVisible();
+    await expect(page.locator('.error-banner')).toContainText(/Full name is required/i);
+
+});
